e57983924ab5c55e7fc70e8acab96f742a04a7ac
Jannes Brunner <mail@jannesbrunner.de>
2022-04-26 18:24:58 +0200

    first alpha version

--- 01_perlin_noise/sketch.js
+++ 01_perlin_noise/sketch.js
@@ -1,9 +1,29 @@
+/// <reference path="./../p5.global-mode.d.ts" />
+
+var inc = 0.01;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background('green');
-    noLoop();
-  }
+  createCanvas(400, 400);
+  pixelDensity(1); // for HiDPI Displays
   
-  function draw() {
-    
+  loadPixels();
+  var yoff = 0;
+  for (let x = 0; x < width; x++) {
+    var xoff = 0; // reset xoff for every x-row in p-array
+    for (let y = 0; y < height; y++) { 
+      var index = (x + y * width) * 4;
+      var r = noise(xoff, yoff) * 255;
+      pixels[index] = r;
+      pixels[index+1] = r;
+      pixels[index+2] = r;
+      pixels[index+3] = 255;  
+      xoff += inc;
+    }
+    yoff += inc;
   }
+  updatePixels();
+}
+
+function draw() {
+// not needed
+}
