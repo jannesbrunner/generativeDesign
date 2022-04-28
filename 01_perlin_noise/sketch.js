296faf5df6f959b789ff133cca7ccd0d153ba2d7
Jannes Brunner <mail@jannesbrunner.de>
2022-04-28 20:06:36 +0200

    add p5gui + major refactore

--- 01_perlin_noise/sketch.js
+++ 01_perlin_noise/sketch.js
@@ -1,52 +1,95 @@
 /// <reference path="./../p5.global-mode.d.ts" />
 
-var inc = 0.009;
-var iterations = 50;
+// global values
+var inc = 0.009 // set incremental step size
+var canvaWidth = 800;
+var canvaHeight = 800;
+
+// Pelin Noise offsets
+var global_xoff = {
+  r : 2000,
+  g : 400,
+  b : 700,
+  a : 0,
+}
+var global_yoff = {
+  r : 2000,
+  g : 400,
+  b : 700,
+  a : 0
+}
 
 function setup() {
-  createCanvas(400, 400);
+
+  createCanvas(canvaWidth, canvaHeight);
   pixelDensity(1); // for HiDPI Displays
+  
+  // set up GUI
+  let guiX = createGui('Offset X');
+  let guiY = createGui('Offset Y');
+  let guiSettings = createGui('Settings');
+  reload = createButton('Reload');
 
-  const repeats = Math.floor(random(0, 50));
-  console.log(`Repeat: ${repeats}`);
-
-  for (let n = 0; n < repeats; n++) {
-    loadPixels();
-    var yr_off = 1000;
-    var yg_off = 300;
-    var yb_off = 0;
-    var ya_off = 6000;
-    for (let x = 0; x < width; x++) {
-      // reset xoff for every x-row in p-array
-      var xr_off = 2000;
-      var xg_off = 400;
-      var xb_off = 700;
-      var xa_off = 0;
-      for (let y = 0; y < height; y++) {
-        var index = (x + y * width) * 4;
-        var r = noise(xr_off, yr_off) * 255;
-        var g = noise(xg_off, yg_off) * 255;
-        var b = noise(xb_off, yb_off) * 255;
-        var a = noise(xa_off, ya_off) * 255;
-        pixels[index] = r;
-        pixels[index + 1] = g;
-        pixels[index + 2] = b;
-        pixels[index + 3] = a;
-        xr_off += inc;
-        xg_off += inc;
-        xb_off += inc;
-        xa_off += inc;
-      }
-      yr_off += inc;
-      yg_off += inc;
-      yb_off += inc;
-      ya_off += inc;
-    }
-    updatePixels();
+  // set positions 
+  const setGuiPositions = () => {
+    reload.position(canvaWidth + 220 , 0);
+    guiX.setPosition(canvaWidth + 10, 200);
+    guiY.setPosition(canvaWidth + 225, 200);
+    guiSettings.setPosition(canvaWidth + 10, 0);
+    reload.size(200, 200);
+    reload.position(canvaWidth + 220 , 0);
   }
+  setGuiPositions();
+  
+  // set offset sliders
+  sliderRange(0, 10000, 100)
+  guiX.addObject(global_xoff);
+  guiY.addObject(global_yoff);
 
+  // set inc slider
+  sliderRange(0.0001, 0.15, 0.0001)
+  guiSettings.addGlobals('inc');
+  
+  // set canva width sliders
+  sliderRange(200, 2000, 100);
+  guiSettings.addGlobals('canvaWidth', 'canvaHeight');
+
+  // reload button
+  reload.mousePressed( () => {
+    resizeCanvas(canvaWidth, canvaHeight);
+    setGuiPositions();
+    drawPattern();
+  }); 
+  drawPattern();
+  // we re-paint with reload button only
+  noLoop();
 }
+
+function drawPattern() {
+  loadPixels();
+  const yoff = { ...global_yoff };
+  for (let x = 0; x < width; x++) {
+    // reset xoff for every x-row in p-array
+    const xoff = { ...global_xoff };
+    for (let y = 0; y < height; y++) {
+      const index = (x + y * width) * 4;
+      let r = noise(xoff.r, yoff.r) * 255;
+      let g = noise(xoff.g, yoff.g) * 255;
+      let b = noise(xoff.b, yoff.b) * 255;
+      let a = noise(xoff.a, yoff.a) * 255;
+      pixels[index] = r;
+      pixels[index + 1] = g;
+      pixels[index + 2] = b;
+      pixels[index + 3] = a;
+      for (let val in xoff) { xoff[val] += inc }
+    }
+    for (let val in yoff) { yoff[val] += inc }
+  }
+  updatePixels();
+}
+
 
 function draw() {
-  // not needed
+ // not needed
 }
+
