029a270f5569f83af000dcf4a7cb7dea0e0dc560
Jannes Brunner <mail@jannesbrunner.de>
2022-05-06 11:23:39 +0200

    Chore: refactoring code

--- 03_spring/sketch.js
+++ 03_spring/sketch.js
@@ -20,66 +20,55 @@ var innerFieldYoff = 100;
 var innerFieldWidth = 200;
 var innerFieldHeight = 200;
 
-
-// settings
+// settings //
 var showFlowField = false;
 var paintMode = true;
 var isPlaying = true;
 var fps = 60;
 
-
-// screen buffers
-var ffb; //flow field buffer
-var ptb; //particle buffer
-var icb; //inner canvas buffer
-var bmb; //flying bee mouse buffer
-
-// Parameters
-var inc = 0.1;
-var zinc = 0.01;
-var scl = 20;
-var cols, rows, innerCols, innerRows;
-var zoff = 0;
-var fr;
-var particles = [];
+// Parameters //
+var inc = 0.1; // incrementer for x,y (p-noise)
+var zinc = 0.01; // incrementer for z (p-noise)
+var scl = 20; // scale / "zoom" factor for flowField
+var cols, rows, innerCols, innerRows; // dimensions for outer and inner flow field
+var zoff = 0; // p-noise
+var fr; // current frame rate
+var particles = []; // particles or bees ;)
 var flowfield;
-const beemax = 100;
-var beecount = 10;
+const beeMax = 100; // max particles
+var beeCount = 10; // current particles
 
-// Pictures
+// Pictures //
 var blossom1;
+var flowers = [];
+var beeR, BeeL, popBackground, popKaboom
+
+
+
+function preload() { 
+  blossom1 = loadImage('images/flower1.png');
+  flowers.push(loadImage('images/Flower_Blue.png'));
+  flowers.push(loadImage('images/Flower_Green.png'));
+  flowers.push(loadImage('images/Flower_Pink.png'));
+  flowers.push(loadImage('images/Flower_Yellow.png'));
+  heading = loadImage('images/Spring_Font.png')
+  beeR = loadImage('images/beeR.png');
+  beeL = loadImage('images/beeL.png');
+  popBackground = loadImage('images/Background.png');
+  popKaboom = loadImage('images/kaboom.png');
+}
 
 function guiSetup() {
   gui = new Gui();
-  gui.panel.addButton("Toggle FlowField", () => {
-    showFlowField = !showFlowField;
-  });
-  gui.panel.addButton("Toogle PaintMode", () => {
-    paintMode = !paintMode;
-  });
+  gui.panel.addButton("Toggle FlowField", () => showFlowField = !showFlowField);
   gui.panel.addButton("Play/Pause", () => {
     isPlaying = !isPlaying;
     isPlaying ? loop() : noLoop();
   });
-  gui.panel.addRange("Bees!", 0, beemax, beecount, 1, (val) => {
-    beecount = val;
-  });
-  gui.panel.addRange("inc", 0.001, 1, inc, 0.001, (val) => {
-    inc = val;
-  });
-  gui.panel.addRange("zinc", 0.0001, 0.5, zinc, 0.001, (val) => {
-    zinc = val;
-  });
-  gui.panel.addRange("FPS", 1, 60, fps, 1, (val) => {
-    fps = val;
-  });
-}
-
-function preload() {
-  blossom1 = loadImage('images/flower1.png');
-  beeR = loadImage('images/beeR.png');
-  beeL = loadImage('images/beeL.png');
-  grass = loadImage('images/grass.jpeg');
+  gui.panel.addRange("Bees!", 0, beeMax, beeCount, 1, (val) => beeCount = val);
+  gui.panel.addRange("inc", 0.001, 1, inc, 0.001, (val) => inc = val);
+  gui.panel.addRange("zinc", 0.0001, 0.5, zinc, 0.001, (val) => zinc = val);
+  gui.panel.addRange("FPS", 1, 60, fps, 1, (val) => fps = val);
 }
 
 function setup() {
@@ -87,31 +76,39 @@ function setup() {
   pixelDensity(2);
   guiSetup();
 
-  ffb = createGraphics(canvaWidth, canvaHeight);
-  ptb = createGraphics(canvaWidth, canvaHeight);
-  icb = createGraphics(innerFieldWidth, innerFieldHeight);
-  bmb = createGraphics(canvaWidth, canvaHeight);
-
+  // Define C/R for flow field grid
   cols = floor(canvaWidth / scl);
   rows = floor(canvaHeight / scl);
+  // Define C/R for inner outgoing flow field grid
   innerCols = floor(flowfieldWidth / scl)
   innerRows = floor(flowfieldHeight / scl)
+  // frame rate paragraph
   fr = createP('');
+  
+  // Create the flowfield
   flowfield = new Array(cols * rows);
-  for (let index = 0; index < beemax; index++) {
+  for (let index = 0; index < beeMax; index++) { // beeMax = max particles
     particles[index] = new Particle(index);
   }
-  background(255);
+ 
+  function shuffleArray(array) { // randomize flower order :) [Fisher-Yates]
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+  flowers = shuffleArray(flowers);
+
 }
 
 function draw() {
-  frameRate(fps);
-  if (paintMode) background(255);
-  bmb.image(beeR, mouseX, mouseY, 20, 20);
-  image(bmb, 0, 0);
-  bmb.clear();
-  image(grass, 0, 0, canvaWidth, canvaHeight);
-  drawInner();
+  frameRate(fps);  
+  image(popBackground, 0, 0, canvaWidth, canvaHeight);
+  image(popKaboom,  innerFieldWidth / 4, innerFieldHeight / 2, (sin(frameCount/5) + 320), sin(frameCount/5) +220);
+  image(heading, innerFieldWidth / 2, innerFieldHeight / 1.3,  200, 100);
   drawOuterFlowers();
 
   for (let y = 0; y < rows; y++) {
@@ -139,48 +136,41 @@ function draw() {
   zoff += zinc;
   fr.html(floor(frameRate()));
 
-  for (var i = 0; i < beecount; i++) {
+  for (var i = 0; i < beeCount; i++) {
     particle = particles[i];
     particle.follow(flowfield)
     particle.update();
     particle.edges();
     particle.show();
   };
-
+  image(beeR, mouseX, mouseY, 20, 20); // let people water plants
 }
 
-function drawInner() {
-  icb.background(255, 0);
-  icb.textFont("Comic Sans MS");
-  icb.textSize(40);
-  icb.fill(128 + sin(frameCount * 0.1) * 128, 60);
-  icb.text('Spring', innerFieldWidth / 4, innerFieldHeight / 2);
-  image(icb, innerFieldWidth / 2, innerFieldHeight / 2);
-  icb.clear();
-}
 
 function drawOuterFlowers() {
+
   // X row top
-  image(blossom1, sin(frameCount/12) + 20, cos(frameCount/12) + 20, 50, 50);
-  image(blossom1, cos(frameCount/10) + 120, sin(frameCount/12) + 25, 50, 50);
-  image(blossom1, sin(frameCount/17) + 230, cos(frameCount/11) + 15, 50, 50);
-  image(blossom1, sin(frameCount/10) + 330, cos(frameCount/18) + 15, 50, 50);
+  image(flowers[0], sin(frameCount/12) + 20, cos(frameCount/12) + 20, 50, 50);
+  image(flowers[1], cos(frameCount/10) + 120, sin(frameCount/12) + 25, 50, 50);
+  image(flowers[2], sin(frameCount/17) + 230, cos(frameCount/11) + 15, 50, 50);
+  image(flowers[3], sin(frameCount/10) + 330, cos(frameCount/18) + 15, 50, 50);
 
   // Y row left
-  image(blossom1, sin(frameCount/15) + 15, cos(frameCount/14) + 120, 50, 50);
-  image(blossom1, cos(frameCount/12) + 25, sin(frameCount/13) + 230, 50, 50);
+  image(flowers[0], sin(frameCount/15) + 15, cos(frameCount/14) + 120, 50, 50);
+  image(flowers[1], cos(frameCount/12) + 25, sin(frameCount/13) + 230, 50, 50);
 
   // Y row right
-  image(blossom1, sin(frameCount/17) + 330, cos(frameCount/14) + 120, 50, 50);
-  image(blossom1, cos(frameCount/13) + 315, sin(frameCount/12) + 230, 50, 50);
+  image(flowers[2], sin(frameCount/17) + 330, cos(frameCount/14) + 120, 50, 50);
+  image(flowers[3], cos(frameCount/13) + 315, sin(frameCount/12) + 230, 50, 50);
 
   // X row bottom
-  image(blossom1, sin(frameCount/15) +  cos(frameCount/14) + 15, 335, 50, 50);
-  image(blossom1, cos(frameCount/12)+ 125, cos(frameCount/10) + 330, 50, 50);
-  image(blossom1,  cos(frameCount/14) + 235, sin(frameCount/15) + 320, 50, 50);
-  image(blossom1, sin(frameCount/16) + 300, cos(frameCount/10) + 330, 50, 50);
+  image(flowers[0], sin(frameCount/15) +  cos(frameCount/14) + 15, 335, 50, 50);
+  image(flowers[1], cos(frameCount/12)+ 125, cos(frameCount/10) + 330, 50, 50);
+  image(flowers[2],  cos(frameCount/14) + 235, sin(frameCount/15) + 320, 50, 50);
+  image(flowers[3], sin(frameCount/16) + 300, cos(frameCount/10) + 330, 50, 50);
 }
 
+// shows the flowField if desired
 function drawFlowField(x, y, v) {
   stroke("red");
   push();
@@ -191,6 +181,7 @@ function drawFlowField(x, y, v) {
   pop();
 }
 
+// determining if we're grid-wise in the inner or outer flow field
 function inInnerFiled(x, y) {
   return ((innerFieldXoff / scl) < x && x < (innerFieldXoff / scl + innerFieldWidth / scl)) &&
     (innerFieldYoff / scl < y && y < (innerFieldYoff / scl + innerFieldHeight / scl))
