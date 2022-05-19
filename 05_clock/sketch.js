80a5e8066089eb2b19ea9759a8e5678d5d9dc412
Jannes Brunner <mail@jannesbrunner.de>
2022-05-19 15:34:51 +0200

    Dusseldorf Clock first alpha ver

--- 05_clock/sketch.js
+++ 05_clock/sketch.js
@@ -1,13 +1,85 @@
 /// <reference path="./../p5.global-mode.d.ts" />
 
-    let angle = 0.1;
+let angle = 0.1;
 
-    let cloud1Pos = -50; 
-    let cloud2Pos = -100;
+let cloud1Pos = -50;
+let cloud2Pos = -100;
+
+// misc
+const dotSpace = 9;
+
+// the clock points
+const seconds9 = [];
+const seconds5 = [];
+const minutes9 = [];
+const minutes5 = [];
+const hours9 = [];
+const hours2 = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
+
+    seconds9.push(
+        { x: -15, y: -8 },
+        { x: -15, y: -8 - 1 * dotSpace },
+        { x: -15, y: -8 - 2 * dotSpace },
+        { x: -15, y: -8 - 3 * dotSpace },
+        { x: -15, y: -8 - 4 * dotSpace },
+        { x: -15, y: -8 - 5 * dotSpace },
+        { x: -15, y: -8 - 6 * dotSpace },
+        { x: -15, y: -8 - 7 * dotSpace },
+        { x: -15, y: -8 - 8 * dotSpace },
+        { x: -15, y: -8 - 9 * dotSpace },
+    )
+
+    seconds5.push(
+        { x: -15, y: -8 - 11 * dotSpace },
+        { x: -15, y: -8 - 12 * dotSpace },
+        { x: -15, y: -8 - 13 * dotSpace },
+        { x: -15, y: -8 - 14 * dotSpace },
+        { x: -15, y: -8 - 15 * dotSpace },
+    )
+
+    minutes9.push(
+
+        { x: -15, y: -8 - 17 * dotSpace },
+        { x: -15, y: -8 - 18 * dotSpace },
+        { x: -15, y: -8 - 19 * dotSpace },
+        { x: -15, y: -8 - 20 * dotSpace },
+        { x: -15, y: -8 - 21 * dotSpace },
+        { x: -15, y: -8 - 22 * dotSpace },
+        { x: -15, y: -8 - 23 * dotSpace },
+        { x: -15, y: -8 - 24 * dotSpace },
+        { x: -15, y: -8 - 25 * dotSpace },
+    )
+
+    minutes5.push(
+        { x: -15, y: -8 - 27 * dotSpace },
+        { x: -15, y: -8 - 28 * dotSpace },
+        { x: -15, y: -8 - 29 * dotSpace },
+        { x: -15, y: -8 - 30 * dotSpace },
+        { x: -15, y: -8 - 31 * dotSpace },
+    )
+
+    hours9.push(
+        { x: -15, y: -8 - 33 * dotSpace },
+        { x: -15, y: -8 - 34 * dotSpace },
+        { x: -15, y: -8 - 35 * dotSpace },
+        { x: -15, y: -8 - 36 * dotSpace },
+        { x: -15, y: -8 - 37 * dotSpace },
+        { x: -15, y: -8 - 38 * dotSpace },
+        { x: -15, y: -8 - 39 * dotSpace },
+        { x: -15, y: -8 - 40 * dotSpace },
+        { x: -15, y: -8 - 41 * dotSpace },
+    )
+
+    hours2.push(
+        { x: -15, y: -8 - 43 * dotSpace },
+        { x: -15, y: -8 - 44 * dotSpace }
+    )
+
+
 }
 
 const scaling = () => {
@@ -27,11 +99,11 @@ function draw() {
 
     //Paint sun
     paintSun();
-    
-    ellipse(cloud1Pos, height/4, 200, 50 * sin(angle - 0.01));
-    ellipse(cloud2Pos - 50, height/5, 250 * sin(angle), 80);
 
-   
+    ellipse(cloud1Pos, height / 4, 200, 50 * sin(angle - 0.01));
+    ellipse(cloud2Pos - 50, height / 5, 250 * sin(angle), 80);
+
+
     // Paint surface
     push();
     translate(0, 0.75 * height);
@@ -46,12 +118,56 @@ function draw() {
     pop();
     // Paint Tower
     paintTower();
+    paintClock();
 
     cloud1Pos += 1;
-    cloud2Pos += 1.2;
+    cloud2Pos += 1.1;
     angle += 1;
-    if(cloud1Pos > width) cloud1Pos = -50;
-    if(cloud2Pos > width) cloud2Pos = -100;
+    if (cloud1Pos > width) cloud1Pos = -50;
+    if (cloud2Pos > width) cloud2Pos = -100;
+
+
+}
+
+function paintClock() {
+
+    let hr = hour();
+    let min = minute();
+    let sec = second();
+
+
+    // Points
+    push();
+    stroke('white');
+    strokeWeight(7);
+    translate(width / 2, 0.88 * height);
+    // seconds 9
+    for (let index = 0; index < sec % 10; index++) {
+        point(seconds9[index].x, seconds9[index].y);
+    }
+    // seconds 5
+    for (let index = 0; index < Math.floor(sec / 10); index++) {
+        point(seconds5[index].x, seconds5[index].y);
+    }
+
+    // minutes 9
+    for (let index = 0; index < min % 10; index++) {
+        point(minutes9[index].x, minutes9[index].y);
+    }
+    // minutes 5
+    for (let index = 0; index < Math.floor(min / 10); index++) {
+        point(minutes5[index].x, minutes5[index].y);
+    }
+    // hours 9
+    for (let index = 0; index <  hr % 10; index++) {
+        point(hours9[index].x, hours9[index].y);
+    }
+    // hours 2
+    for (let index = 0; index < Math.floor(hr / 10); index++) {
+        point(hours2[index].x, hours2[index].y);
+    }
+    pop();
+
 }
 
 function paintSun() {
@@ -68,21 +184,20 @@ function paintSun() {
 }
 
 function paintTower() {
-
     push();
-    translate(width / 2, 0.75 * height);
+    translate(width / 2, 0.88 * height);
     fill(48, 48, 48, 255);
     scale(scaling());
     beginShape();
-    vertex(-50, 0)
-    vertex(-50, -250)
-    vertex(-75, -280)
-    vertex(-25, -280)
-    vertex(-15, -350)
-    vertex(0, -280)
-    vertex(50, -280)
-    vertex(25, -250)
-    vertex(25, 0)
+    vertex(-55, 0)
+    vertex(-50, -330)
+    vertex(-75, -350)
+    vertex(-25, -350)
+    vertex(-15, -390)
+    vertex(0, -350)
+    vertex(50, -350)
+    vertex(25, -330)
+    vertex(30, 0)
     endShape();
     pop();
 
